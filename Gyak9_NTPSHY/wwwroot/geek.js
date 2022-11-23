8a80ff3ecb946f05ceffe5936430df97b3c8ee5f
TheReif <reifmarton@gmail.com>
2022-11-23 15:18:28 +0100

    mukszik

--- Gyak9_NTPSHY/wwwroot/geek.js
+++ Gyak9_NTPSHY/wwwroot/geek.js
@@ -1,5 +1,27 @@
-﻿var viccek;
+﻿console.onload = () => {
+    console.log("Oldal betöltve");
+
+}
+
+
+var viccek;
 function letöltés() {
     fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
 
+function letöltésBefejeződött(d) {
+    console.log("Sikekes letöltés")
+    console.log(d)
+    viccek = d;
+    
+
+    for (var i = 0; i < viccek.length; i++) {
+        var új = document.createElement("dev");
+        új.innerHTML = viccek[i].text;
+
+        document.getElementById("ide").appendChild(új);
+    }
 }
